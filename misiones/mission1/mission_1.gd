aebe1b04f4b90759dc5d49a9e3fd52404fffe1ef
mspadron28 <matiaspadron2@gmail.com>
2025-02-09 11:21:34 -0500

    Funcionalidad Tita iniciada y cambio en el diseño de cada hud tanto de Bebop como Tita

--- misiones/mission1/mission_1.gd
+++ misiones/mission1/mission_1.gd
@@ -1,11 +1,64 @@
 extends Node2D
 
-@onready var bebop: Node2D = $Bebop
+@export var BebopScene: PackedScene
+@export var TitaScene: PackedScene
 
+var current_player: Node2D
+var bebop: Node2D
+var tita: Node2D
+var tita_alive: bool = true
 
 func _ready() -> void:
+	# Instanciar los personajes desde las escenas exportadas
+	bebop = BebopScene.instantiate()
+	tita = TitaScene.instantiate()
+	add_child(bebop)
+	add_child(tita)
+
+	# Inicialmente, Bebop es el personaje activo
+	current_player = bebop
+	tita.visible = false  # Oculta a Tita al inicio
+	tita.set_physics_process(false)  # Deshabilita el procesamiento de física de Tita al inicio
+	tita.set_process_input(false)  # Deshabilita la entrada de Tita
+	tita.set_process(false)  # Deshabilita cualquier otro proceso de Tita
+	tita.get_node("CanvasLayer").visible = false  # Oculta el HUD de Tita al inicio
+
 	bebop.tree_exiting.connect(func():
 		await get_tree().create_timer(1).timeout
 		get_tree().change_scene_to_file("res://menus/game_over.tscn")
 	)
-	
+
+	# Conectar el evento de salud de Tita para cambiar automáticamente a Bebop si Tita muere
+	var tita_stats = tita.get_node("StatsComponent")
+	tita_stats.health_changed.connect(func():
+		if tita_stats.health <= 0:
+			tita_alive = false
+			switch_character(bebop)
+	)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_up") and tita_alive:
+		switch_character(tita)
+	elif Input.is_action_just_pressed("ui_down"):
+		switch_character(bebop)
+
+func switch_character(new_player: Node2D) -> void:
+	if new_player != current_player and new_player != null:
+		current_player.visible = false  # Oculta el personaje actual
+		current_player.set_physics_process(false)  # Deshabilita el procesamiento de física
+		current_player.set_process_input(false)  # Deshabilita la entrada
+		current_player.set_process(false)  # Deshabilita cualquier otro proceso
+		current_player.get_node("CanvasLayer").visible = false  # Oculta el HUD del personaje actual
+
+		new_player.visible = true  # Muestra el nuevo personaje
+		new_player.set_physics_process(true)  # Habilita el procesamiento de física
+		new_player.set_process_input(true)  # Habilita la entrada
+		new_player.set_process(true)  # Habilita cualquier otro proceso
+		new_player.position = current_player.position  # Mantiene la posición actual
+		new_player.get_node("CanvasLayer").visible = true  # Muestra el HUD del nuevo personaje
+
+		current_player = new_player  # Cambia la referencia del personaje activo
+
+
+func _on_retornar_pressed() -> void:
+	get_tree().change_scene_to_file("res://menus/principal/menu.tscn")
---
+++ players_sprite/player_tita/tita.gd
@@ -0,0 +1,55 @@
+class_name Tita
+extends Node2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var move_component: MoveComponent = $MoveComponent
+@onready var left_muzzle: Marker2D = $LeftMuzzle
+@onready var spawner_component: SpawnerComponent = $SpawnerComponent
+@onready var fire_rate_timer: Timer = $FireRateTimer
+@onready var scale_component: ScaleComponent = $ScaleComponent
+@onready var right_muzzle: Marker2D = $RightMuzzle
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
+@onready var flash_component: FlashComponent = $FlashComponent
+@onready var shake_component: ShakeComponent = $ShakeComponent
+#Para los corazones
+@onready var stats_component: StatsComponent = $StatsComponent
+@onready var life_node: Control = $CanvasLayer/Life
+@onready var bebop_fire: VariablePitchAudioStreamPlayer = $BebopFire
+
+
+func _ready() -> void:
+# Conecta los cambios de salud con la actualización de los corazones
+	stats_component.health_changed.connect(update_hearts)
+# Inicializa los corazones según la salud máxima
+	update_hearts()
+	fire_rate_timer.timeout.connect(fire_lasers)
+	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent):
+		scale_component.tween_scale()
+		flash_component.flash()
+		shake_component.tween_shake()
+	)
+
+func fire_lasers() -> void:
+	bebop_fire.play_with_variance()
+	spawner_component.spawn(left_muzzle.global_position)
+	spawner_component.spawn(right_muzzle.global_position)
+	scale_component.tween_scale()
+
+func _process(delta: float) -> void:
+	animate_the_ship()
+
+func animate_the_ship() -> void:
+	if move_component.velocity.x < 0:
+		animated_sprite_2d.play("bank_left")
+	elif move_component.velocity.x > 0:
+		animated_sprite_2d.play("bank_right")
+	else:
+		animated_sprite_2d.play("center")
+		
+# Actualiza la visualización de los corazones
+func update_hearts() -> void:
+	var current_health = stats_component.health
+	life_node.on_player_life_changed(current_health)
+	
+func heal(amount: int) -> void:
+	stats_component.health += amount
+	update_hearts() 
