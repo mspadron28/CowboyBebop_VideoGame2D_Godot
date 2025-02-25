12935d2695c6fe833e4b465d7a89c0c8f8de6e49
mspadron28 <matiaspadron2@gmail.com>
2025-02-25 00:39:27 -0500

    Fin... :,)

--- menus/fin_mision4/menu.gd
+++ menus/fin_mision4/menu.gd
@@ -2,10 +2,10 @@ extends Control
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene_to_file("res://menus/principal/menu.tscn")
+		get_tree().change_scene_to_file("res://menus/extras/creditos/credits.tscn")
 
 func _on_final_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://menus/principal/menu.tscn")
+	get_tree().change_scene_to_file("res://menus/extras/creditos/credits.tscn")
 
 
 func _on_salir_pressed() -> void:
